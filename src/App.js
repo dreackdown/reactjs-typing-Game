542e53c13f8ef97771d1ad60638c37ad6152612b
Hugo Faria <hugofaria157@.live.com>
2021-09-13 20:10:36 -0300

    dia 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+    return (
+    <div className="container">
+        <div className="valid-keys">
+        <span className="matched">hu</span>  
+        <span className="remainder">go</span>        
+        </div>
+        <div className="typed-keys">asdfhuasdf</div>
+        <div className="completed-words">
+            <ol>
+                <li>cidade</li>
+                <li>carro</li>
+                <li>profissional</li>
+            </ol>
+        </div>
     </div>
-  );
-}
+    );
+};
 
-export default App;
+export default App;   
